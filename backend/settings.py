3733d2f641f1c1576d8499c71abf4c112e87a921
SithabileM <sithabilemonde9@gmail.com>
2025-07-24 13:15:06 +0200

    remove media root and media url from settings file

--- backend/settings.py
+++ backend/settings.py
@@ -139,8 +139,10 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Image files
+"""
 MEDIA_ROOT=BASE_DIR / 'media'
 MEDIA_URL='/media/'
+"""
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
